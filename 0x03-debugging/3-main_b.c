ab9bc05bfd93bc36558752c9a0ed8a8eee1f8830
UnlockMirara <mnjihia66@gmail.com>
2023-06-21 05:33:28 -0700

    task 3

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,25 +2,29 @@
 #include "main.h"
 
 /**
- * main - takes a date and prints how many days are left in the year, taking
- * leap years into account
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Description: This function demonstrates the usage of the convert_day()
+ *              and print_remaining_days() functions by printing a date
+ *              and its remaining days.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int month;
-	int day;
-	int year;
+int month;
+int day;
+int year;
 
-	month = 2;
-	day = 29;
-	year = 2000;
+month = 2;
+day = 29;
+year = 2000;
 
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
+printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-	day = convert_day(month, day);
+day = convert_day(month, day);
 
-	print_remaining_days(month, day, year);
+print_remaining_days(month, day, year);
 
 return (0);
 }
