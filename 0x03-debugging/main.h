0facc6357b602f39b778d213ed04c306e7dce7b2
UnlockMirara <mnjihia66@gmail.com>
2023-06-21 02:01:55 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,13 @@
 
 void positive_or_negative(int i);
 
+/*Function that give the largest number of three*/
+int largest_number(int a, int b, int c);
+
+/*Function that convert day*/
+int convert_day(int month, int day);
+
+/*Function that print remaining*/
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
